fc9d37141300f61fffe9b55f84dc4e0bc5bf72e5
j-ulienguyen <j-ulienguyen@hotmail.com>
2019-11-03 12:05:24 -0800

    Completed Level Up Modal

--- App.js
+++ App.js
@@ -6,16 +6,17 @@ import styles from './app/styles/AppStyles';
 import HomeScreen from './app/screens/HomeScreen';
 import MissionsScreen from './app/screens/MissionsScreen';
 
-import RewardModal from './app/comps/RewardModal';
-
+// import RewardModal from './app/comps/RewardModal';
+import LevelUpModal from './app/comps/LevelUpModal';
 
 export default function App(){
 	// UI
 	return (
 		<View style={styles.container}>
-			<RewardModal/>
-			{/* <HomeScreen/> */}
+			{/* <RewardModal/> */}
+			<HomeScreen/>
 			{/* <MissionsScreen/> */}
+			<LevelUpModal/>
 		</View>
 	)
 }
