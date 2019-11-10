8306008909573be118361dd5f5255d8a871752e7
j-ulienguyen <j-ulienguyen@hotmail.com>
2019-11-10 12:40:34 -0800

    Installed RNRF + create Route.js

--- App.js
+++ App.js
@@ -3,52 +3,23 @@ import {View, Text} from 'react-native';
 
 // Import comps & styles below
 import styles from './app/styles/AppStyles';
-import SigninScreen from './app/screens/SigninScreen';
-import HomeScreen from './app/screens/HomeScreen';
-import MissionsScreen from './app/screens/MissionsScreen';
-import LeaderboardScreen from './app/screens/LeaderboardScreen'
-import ProfileScreen from './app/screens/ProfileScreen';
-import RewardModal from './app/comps/RewardModal';
-import LevelUpModal from './app/comps/home/LevelUpModal';
-import BadgeModal from './app/comps/profile/BadgeModal';
-import SignupScreen from './app/screens/SignupScreen';
-import OnboardingScreen from './app/screens/OnboardingScreen';
 
+// Navigation
+import Route from './Route';
 
-export default from './storybook';
 
 function App(){
 	// UI
 	return (
 		<View style={styles.container}>
-			{/* <SigninScreen /> */}
-			{/* <SignupScreen /> */}
-			{/* <OnboardingScreen /> */}
-
-			{/* <LeaderboardScreen /> */}
-			{/* <MissionsScreen /> */}
-			{/* <SignupScreen /> */}
-			{/* <SigninScreen/> */}
-
-			{/* <HomeScreen/> */}
-
-			{/* <RewardModal
-				heading = "Woo-hoo!"
-				description = "Thank you for joining EcoHero. Please accept these rewards as a token of appreciation"
-				imagePath = {require('./app/assets/imgs/star-reward.png')}
-				starAmount = {5}
-				xpAmount = {100}
-			/> */}
-
-      		{/* <MissionsScreen/> */}
-			{/* <LevelUpModal level="5"/> */}
-			{/* <ProfileScreen/> */}
-
-			{/* <BadgeModal
-				heading = "Eco-friendly"
-				imagePath = {require('./app/assets/imgs/eco-badge.png')}
-				description = "You've unlocked a badge for completing your first eco mission"
-			/> */}
+			<Route/>
 		</View>
 	)
 }
+
+
+// To view App -> Uncomment below
+export default App;
+
+// To view Storybook -> Uncomment below
+// export default from './storybook';
---
+++ Route.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Router, Stack, Scene} from 'react-native-router-flux';
+
+// Import Screens comps below
+import SigninScreen from './app/screens/SigninScreen';
+import SignupScreen from './app/screens/SignupScreen';
+import OnboardingScreen from './app/screens/OnboardingScreen';
+import HomeScreen from './app/screens/HomeScreen';
+import MissionsScreen from './app/screens/MissionsScreen';
+import LeaderboardScreen from './app/screens/LeaderboardScreen';
+import ProfileScreen from './app/screens/ProfileScreen';
+
+// Import Subscreens comps below
+import BasicProfile from './app/subscreens/BasicProfileSubscreen';
+import ChooseAvatar from './app/subscreens/ChooseAvatarSubscreen';
+import ForgotPW from './app/subscreens/ForgotPWSubscreen';
+
+
+export default function Route(){
+    return (
+        <Router>
+            <Stack key="root" hideNavBar={true}>
+                <Scene
+                    key = "signin"
+                    title = "Signin Screen"
+                    component = {SigninScreen}
+                    initial = {true} // App will start with this screen
+                />
+                <Scene
+                    key = "signup"
+                    title = "Signup Screen"
+                    component = {SignupScreen}
+                />
+                <Scene
+                    key = "onboard"
+                    title = "Onboarding Screen"
+                    component = {OnboardingScreen}
+                />
+                <Scene
+                    key = "home"
+                    title = "Home Screen"
+                    component = {HomeScreen}
+                />
+                <Scene
+                    key = "missions"
+                    title = "Missions Screen"
+                    component = {MissionsScreen}
+                />
+                <Scene
+                    key = "leaderboard"
+                    title = "Leaderboard Screen"
+                    component = {LeaderboardScreen}
+                />
+                <Scene
+                    key = "profile"
+                    title = "Profile Screen"
+                    component = {ProfileScreen}
+                />
+            </Stack>
+        </Router>
+    )
+};
