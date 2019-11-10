a7eab65da2ab48f7de735fce3c5b04ccff7f2b78
j-ulienguyen <j-ulienguyen@hotmail.com>
2019-11-10 11:14:27 -0800

    WIP filter tab menu

--- app/comps/FilterTabMenu.js
+++ app/comps/FilterTabMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
 
 // Import comps & styles below
@@ -6,6 +6,28 @@ import styles from '../styles/MissionBoardCardStyles';
 
 
 export default function FilterTabMenu({tab1, tab2, tab3}){
+
+    const [toggleTab1, setToggleTab1] = useState(true);
+    const [toggleTab2, setToggleTab2] = useState(false);
+    const [toggleTab3, setToggleTab3] = useState(false);
+
+    if(toggleTab1 === true){
+        activeFilter = styles.filterBtnActive
+
+        setToggleTab2(!toggleTab2);
+        setToggleTab3(!toggleTab3);
+    }
+
+    if(toggleTab2 === true){
+        setToggleTab1(!toggleTab1);
+        setToggleTab3(!toggleTab3);
+    }
+
+    if(toggleTab3 === true){
+        setToggleTab1(!toggleTab1);
+        setToggleTab2(!toggleTab2);
+    }
+
 
     function FilterButton({activeFilter, title}){
         return (
@@ -18,7 +40,13 @@ export default function FilterTabMenu({tab1, tab2, tab3}){
     // UI
     return (
         <View style={styles.filterContainer}>
-            <FilterButton title={tab1} activeFilter={styles.filterBtnActive}/>
+            <FilterButton
+                title={tab1}
+                activeFilter={styles.filterBtnActive}
+                //onPress={()=>{
+                  //  setToggleTab1(!toggleTab1);
+                //}}
+            />
             <FilterButton title={tab2} activeFilter={null}/>
             <FilterButton title={tab3} activeFilter={null}/>
         </View>
