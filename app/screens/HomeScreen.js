03d9da18c9e1916e4b865cfaf1b5b809dfdd005c
j-ulienguyen <j-ulienguyen@hotmail.com>
2019-11-28 01:43:01 -0800

    User level updates depending on XP

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -46,13 +46,11 @@ export default function HomeScreen(){
 		var user_id = await AsyncStorage.getItem("user_id");
 		user_id = parseInt(user_id);
 
-		var availableMissions;
-
 		try {
 			var user = await ax("users_read", {id:user_id});
-
 			var missions = await ax("missions_read", {user_id:user_id});
 
+			var availableMissions;
 			// Filter all missions to only get active bonus and normal missions
 			// Type > 0 will ignore missions in the table that is mission_type=-1
 			availableMissions = missions.filter((obj, i)=>{
@@ -107,19 +105,41 @@ export default function HomeScreen(){
 			// Show current amount of available missions for the user
 			// console.log("Current # of Available Missions: ", user[0].mission_available);
 
+
+			// User Level Up
+			// This is the amount of XP required for each level -> Level:XP
+			var xp_required = {
+				1: 0,
+				2: 50,
+				3: 150,
+				4: 250,
+				5: 350,
+				6: 450
+			}
+
+			var currentLevel;
+
+			// Loop through to determine user's current level
+			for (var level in xp_required){
+				if (user[0].xp_amount >= xp_required[level]){
+					currentLevel = level;
+					console.log("Loop -> Current Level: ", currentLevel);
+				}
+			}
+
+			// Set user level
+			user[0].level = currentLevel;
+			console.log("User Level: ", user[0].level);
+			console.log("Current XP: ", user[0].xp_amount);
+
+
 			// Set User Data
 			setUserData(user[0]);
 		} catch (error){
-			console.log("Error GetUserData")
+			console.log("Error GetUserData", error.message);
 		}
 		// console.log("End of GetUserData");
 	}
-
-
-	// Load once
-    useEffect(()=>{
-		GetUserData();
-	}, [])
 
 
 	var username = userData.username;
@@ -146,8 +166,37 @@ export default function HomeScreen(){
 		// console.log("End of UpdateUserData");
 	}
 
-	// Call function to update user data inside db
-	UpdateUserData();
+
+
+	// // User Level Up
+	// function GetUserLevel(currentXP, currentLevel){
+	// 	// This is the amount of XP required for each level -> Level:XP
+	// 	var xp_required = {
+	// 		1: 0,
+	// 		2: 50,
+	// 		3: 150,
+	// 		4: 250,
+	// 		5: 350,
+	// 		6: 450
+	// 	}
+
+	// 	// Loop through to determine user's current level
+	// 	for (var level in xp_required){
+	// 		if (currentXP >= xp_required[level]){
+	// 			currentLevel = level;
+	// 			console.log("Loop -> Current Level: ", currentLevel);
+	// 		}
+	// 	}
+
+	// 	// Set user level
+	// 	// userLevel was stated outside of function -> userData.level
+	// 	userLevel = currentLevel;
+	// 	console.log("User Level: ", userLevel);
+	// 	console.log("Current XP: ", userXP);
+	// }
+
+	// GetUserLevel(userXP, userLevel);
+
 
 
 	// User Star Count and Prize Cards
@@ -205,79 +254,14 @@ export default function HomeScreen(){
 	}
 
 
-	// User Level Up
-	// Level 1: 0
-	// Level 2: 50
-	// Level 3: 150
-	// Level 4: 250
-	// Level 5: 350
-	// Level 6: 450
-
-	console.log("Current XP: ", userXP);
+	// Load once
+    useEffect(()=>{
+		GetUserData();
+	}, [])
 
 
-	// function UserLevelUp(currentXP){
-	// 	var xp_required = [0, 50, 150, 250, 350, 450]; // Level: 1, 2, 3, 4, 5, 6
-	// 	var maxLevel = xp_required.length;
-
-	// 	for(var level = 0; level < maxLevel; level++){
-	// 		if (xp_required[level] > currentXP){
-	// 			return level;
-	// 		}
-	// 	}
-
-	// 	return maxLevel;
-	// }
-
-	//UserLevelUp(userXP);
-
-
-	function GetUserLevel(currentXP, currentLevel){
-		// This is the amount of XP required for each level
-		// Level:XP
-		var xp_required = {
-			1: 0,
-			2: 50,
-			3: 150,
-			4: 250,
-			5: 350,
-			6: 450
-		}
-
-		// Loop through to determine user's current level
-		for (var level in xp_required){
-			if (currentXP >= xp_required[level]){
-				currentLevel = level;
-				console.log("Current Level: ", currentLevel);
-			}
-		}
-	}
-
-	GetUserLevel(userXP, userLevel);
-
-
-	// if (userXP == 50){
-	// 	userLevel = 2;
-	// }
-
-	// else if (userXP == 150){
-	// 	userLevel = 3;
-	// }
-
-	// else if (userXP == 250){
-	// 	userLevel = 4
-	// }
-
-	// else if (userXP == 350){
-	// 	userLevel = 5
-	// }
-
-	// else if (userXP == 450){
-	// 	userLevel = 6
-	// }
-
-	//setToggleModal(true);
-
+	// Call function to update user data inside db
+	UpdateUserData();
 
 
     // UI
@@ -350,7 +334,6 @@ export default function HomeScreen(){
 					level = {userLevel}
 					onPress = {()=>{
 						setToggleModal(false); // Close modal
-						userXP = userXP + 1;
 					}}
 				/>
 			</Modal>
